3d754b42e2a6d0148a2cf9df929e4fabdb57f9e6
vtu25076-coder <vtu25076@veltech.edu.in>
2026-02-06 10:20:09 +0530

    Time: 9 ms (82.29%) | Memory: 47 MB (93.68%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = names.length;
+
+        // Create index array
+        Integer[] index = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            index[i] = i;
+        }
+
+        // Sort indexes based on heights (descending)
+        Arrays.sort(index, (a, b) -> heights[b] - heights[a]);
+
+        // Build result
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = names[index[i]];
+        }
+
+        return result;
+    }
+}
+
+
